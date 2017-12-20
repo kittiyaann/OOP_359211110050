bf3ae832b971da61e34318e05d81d7da186f3678
kittiyaann <annyouma21@gmail.com>
2017-12-20 17:54:50 +0700

    StudentData

---
+++ src/oopLab4/StudentData.java
@@ -0,0 +1,23 @@
+package oopLab4;
+//เขียนโปรแกรมเพื่อรับค่าข้อมูลของนักเรียนหนึ่งคนโดยมีข้อมูลดังนี้
+//1.ชื่อ-สกุล
+//2.รหัสนักศึกษา
+//3.สาขาที่เรียนและคณะ
+//4.ที่อยู่
+//5.email
+//หมายเหตุ : ต้องรับค่าข้อมูลแต่ละคนโดยการใช้ Methods
+
+public class StudentData {
+    static  BufferdReadre reader = new BufferdReadre{
+        System.out.print("Emter you ");
+    }
+    public static void  main(System[]args){
+        getName();
+    }//main
+
+    private static void getName() {
+    }
+
+    private static class BufferdReadre {
+    }
+}//Class
