291920ed4343338ab12ae5a6e65fdc2bc2f20780
kitttiyaann <annyouma21@gmail.com>
2017-12-29 10:10:50 +0700

    myStrBauff

---
+++ src/ooplab6/myStrToken.java
@@ -0,0 +1,21 @@
+package ooplab6;
+
+import java.util.StringTokenizer;
+
+public class myStrToken {
+    public static void main (String []asgr){
+       //StringToKenizer
+        String msg ="Hello Welcome MT MTRMUTSV 2017";
+        StringTokenizer myToKen = new StringTokenizer(msg);
+        //countToKen()
+        System.out.println(myToKen.countTokens());
+
+        while (myToKen.hasMoreTokens()){
+            StringBuffer buffer = new StringBuffer(myToKen.nextToken());
+            System.out.print(buffer.reverse()+" ");
+
+        }//while
+
+
+    }//main
+}//class
